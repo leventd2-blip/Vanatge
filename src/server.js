65717d84f16ed3e2ffeff1ffd9cda23cd4a02bc6
EAufDie1 <levent.d2@schule.bremen.de>
2026-08-11 19:25:19 +0200

    Use environment variable for server port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const { getDb, saveDb } = require('./database');
 
 function startServer(client) {
     const app = express();
-    const PORT = 3000;
+    const PORT = process.env.PORT || 3000;
 
     app.get('/callback', async (req, res) => {
         const code = req.query.code;
