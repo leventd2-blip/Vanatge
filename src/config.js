97653121bae15d1e981db034349722eacef5c3cf
EAufDie1 <levent.d2@schule.bremen.de>
2026-08-11 19:43:10 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
-require('dotenv').config();
-
 module.exports = {
     token: process.env.TOKEN,
     clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI,
     guildId: process.env.GUILD_ID,
     verifiedRoleId: process.env.VERIFIED_ROLE_ID,
-    owners: process.env.OWNERS ? process.env.OWNERS.split(',').map(id => id.trim()) : [],
-    verifyUrl: process.env.VERIFY_URL
+    owners: process.env.OWNERS ? process.env.OWNERS.split(',').map(id => id.trim()) : []
 };
